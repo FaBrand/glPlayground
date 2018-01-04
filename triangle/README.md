69b8c42953fab18bf79b437bdcf8d7344a546d06
FaBrand <21087362+FaBrand@users.noreply.github.com>
2018-01-04 01:11:23 +0100

    Added triangle using legacy OpenGl
    Change-Id: I08497b43745ad24ba2219aabe27ac7fdefb73a93

---
+++ triangle/README.md
@@ -0,0 +1,7 @@
+# Triangles
+
+A basic Window that only draws a default triangle on the window.
+May draw nothing at all depending on the default Shaders defined (or possibly not defined) by the Graphics driver.
+
+Link to [Youtube Tutorial](https://www.youtube.com/watch?v=H2E3yO0J7TM) followed to do this.
+
--- triangle/src/application.cpp
+++ triangle/src/application.cpp
@@ -1,4 +1,43 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
 int main(int, char**)
 {
+    if (!glfwInit())
+    {
+        std::cout << "Could not initialize glfw" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "Could not create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     return 0;
 }
