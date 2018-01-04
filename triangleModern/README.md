e9f41e97cdd33c3a87961bcc76be055d113eb2a1
FaBrand <21087362+FaBrand@users.noreply.github.com>
2018-01-04 01:21:22 +0100

    Added empty window for new target

---
+++ triangleModern/README.md
@@ -0,0 +1,7 @@
+# Triangles
+
+A basic Window that only draws a default triangle on the window using vertex buffers.
+May draw nothing at all depending on the default Shaders defined (or possibly not defined) by the Graphics driver.
+
+Link to [Youtube Tutorial](https://www.youtube.com/watch?v=0p9VxImr7Y0) followed to do this.
+
---
+++ triangleModern/src/application.cpp
@@ -0,0 +1,37 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main(int, char**)
+{
+    if (!glfwInit())
+    {
+        std::cout << "Could not initialize glfw" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "Could not create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
